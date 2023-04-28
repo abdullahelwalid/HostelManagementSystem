619702ef074a7794cac9ac15b53d2e6693e740be
sayya2 <ninetail86@gmail.com>
2023-04-28 17:26:13 +0800

    init

---
+++ src/hostel/User.java
@@ -0,0 +1,36 @@
+
+package hostel;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author ninet
+ */
+public class User {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phoneNum;
+    public String username;
+    private String password;
+    public int id;
+    
+    public void login(String username, String password){
+        
+    } 
+    
+   
+    
+//    public static void main (String[]args){
+//        Scanner sc = new Scanner (System.in);
+//        
+//        System.out.println("Type your username: ");
+//        String username = sc.nextLine();
+//        System.out.println(username);
+//        
+//}
+}
---
+++ src/hostel/application.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hostel;
+
+/**
+ *
+ * @author ninet
+ */
+public class application {
+    
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
